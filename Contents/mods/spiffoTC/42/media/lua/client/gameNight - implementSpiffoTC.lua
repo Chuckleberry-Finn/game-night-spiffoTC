b23d3de961b5ce34afb1d646ad243edffb17fe6f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-01 16:56:10 -0500

    B42

---
+++ Contents/mods/spiffoTC/42/media/lua/client/gameNight - implementSpiffoTC.lua
@@ -0,0 +1,37 @@
+local applyItemDetails = require "gameNight - applyItemDetails"
+local deckActionHandler = applyItemDetails.deckActionHandler
+local gamePieceAndBoardHandler = applyItemDetails.gamePieceAndBoardHandler
+
+local spiffoCards = {
+    "DOCTOR", "ELECTRICIAN", "ENGINEER", "FARMER",
+    "FIRE OFFICER", "FISHERMAN", "FITNESS INSTRUCTOR",
+    "LUMBERJACK", "MECHANIC", "METALWORKER", "NURSE",
+    "PARK RANGER", "POLICE OFFICER", "REPAIRMAN",
+    "SECURITY GUARD", "UNEMPLOYED", "VETERAN",
+    "BURGER FLIPPER", "BURGLAR", "CARPENTER",
+    "CHEF", "CONSTRUCTION WORKER"
+}
+deckActionHandler.addDeck("SpiffoCards", spiffoCards)
+gamePieceAndBoardHandler.registerSpecial("Base.SpiffoCards", { applyCards = "applyCardForSpiffo", actions = {examine=true}, examineScale = 1, textureSize = {176,250} })
+
+
+
+function applyItemDetails.applyCardForSpiffo(item)
+    if not item:getModData()["gameNight_cardDeck"] then
+
+        local itemCont = item:getContainer()
+        local zombie = itemCont and (itemCont:getType() == "inventorymale" or itemCont:getType() == "inventoryfemale")
+
+        local stack = zombie and 1 or ZombRand(3)+1
+        local cards = {}
+
+        for i=1, stack do
+            local card = spiffoCards[ZombRand(#spiffoCards)+1]
+            table.insert(cards, card)
+        end
+
+        item:getModData()["gameNight_cardDeck"] = cards
+        item:getModData()["gameNight_cardFlipped"] = {}
+        for i=1, #cards do item:getModData()["gameNight_cardFlipped"][i] = true end
+    end
+end
---
+++ Contents/mods/spiffoTC/42/media/lua/shared/gameNight - DistributionsSpiffoTC.lua
@@ -0,0 +1,23 @@
+require "Items/SuburbsDistributions"
+
+local gameNightDistro = require "gameNight - Distributions"
+
+gameNightDistro.proceduralDistGameNight.itemsToAdd["SpiffoCards"] = {
+    rolls = 4,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
+
+local addToInsert = {"CrateSpiffoMerch","ServingTrayBurgers", "ServingTrayChickenNuggets",
+                     "ServingTrayFries", "ServingTrayOnionRings", "SpiffosDiningCounter",
+                     "SpiffosKitchenBags", "SpiffosKitchenCounter",}
+
+for _,contID in pairs(addToInsert) do
+    gameNightDistro.proceduralDistGameNight.listsToInsert[contID] = { generalChance = 0, chanceOverride = {["SpiffoCards"] = 10 } }
+end
+
+
+
